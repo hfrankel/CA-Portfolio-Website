ef2b1c6f689f82eedb4e2acdc154b78207644e09
Harry Frankel <hfranks92@gmail.com>
2021-03-09 11:23:21 +1100

    Refactor html css reset and nav script position. Minimise nav on link click

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    //Toggle Nav
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+      if (nav.classList.contains('nav-active')) {
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+            link.style.animation = '';
+          } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 1
+            }s`;
+          }
+        });
+      }
+    });
+  }
+
+  //Toggle Nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
 
     //Animate Links
     navLinks.forEach((link, index) => {
-      if (link.style.animation){
-        link.style.animation = ''
+      if (link.style.animation) {
+        link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
       }
     });
   });
-}
+};
 
 navSlide();
